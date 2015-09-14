084d881915336b320166d8da252088a95fceb20f
rani2015 <rani.learn2015@gmail.com>
2015-09-14 12:11:11 -0400

    commit

--- users/rani/spring_institute/src/com/institute/dao/TrainerDAO.java
+++ users/rani/spring_institute/src/com/institute/dao/TrainerDAO.java
@@ -7,7 +7,7 @@ import com.institute.model.Trainer;
 public interface TrainerDAO {
 public Trainer save(Trainer trainer);
 public Trainer getById(int id);
-//public List<Trainee> getByTrainerId(int trainerId);
+
 public Trainer getByTrainerId(int trainerId);
 public void delete(Trainer trainer);
 public List<Trainer> findAll();
